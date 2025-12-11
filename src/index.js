4a968293df2debaf2a7dbcf3788ec3cb69bdad68
Gregor MacLennan <gmaclennan@awana.digital>
2025-12-11 17:58:14 +0000

    add SecretStreamSocket type export

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ export { fetch } from './fetch.js'
 export { createServer } from './server.js'
 export { setGlobalDispatcher, getGlobalDispatcher } from './global.js'
 export { Agent } from './agent.js'
+
+/** @typedef {import('./secret-stream-socket.js').SecretStreamSocket} SecretStreamSocket */
